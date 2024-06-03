fd7ef7ed39bb87e6f4bd68ae76b5d0fa62c593b0
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-06-03 15:04:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,4 @@
- Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
-import re
-from os import environ,getenv
-from Script import script 
-
-id_pattern = re.compile(r'^.\d+$')
+ id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -17,11 +8,10 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'MOVIES')
-API_ID = int(environ.get('API_ID', '24491383'))
-API_HASH = environ.get('API_HASH', '78e18eba669cc519ffd7a3c89f9ed32a')
+SESSION = environ.get('SESSION', 'TechVJBot')
+API_ID = int(environ.get('API_ID', '22182189'))
+API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
-
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/c7ac07501e857860334e7.jpg https://telegra.ph/file/943126cf288c36e0e086d.jpg https://telegra.ph/file/63b6dc593585363ded68d.jpg')).split() #SAMPLE PIC
