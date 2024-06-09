b352e408a146d569a29b4308a3f1902ff00c6e4e
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-06-09 16:49:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -211,22 +211,17 @@ Nᴀᴍᴇ - {}"""
 <b> Size ⚙️: {file_size}</b>""" 
 
     IMDB_TEMPLATE_TXT = """
-<b>Query: {qurey}
 
-IMDb Data:
-
+<b>MASSAGE: {qurey}
 <b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
+🎙️ Languages : <code>{languages}</code>
 📀 RunTime: {runtime} Minutes
 📆 Release Info : {release_date}
 🎛 Countries : <code>{countries}</code>
-
-
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
-
 Requested by : {message.from_user.mention}</b>"""
     
     ALL_FILTERS = """
